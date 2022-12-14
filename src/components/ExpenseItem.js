d2ac2d0252ab57eda57786566412fdbfa335f7e9
Aida Šertović Komić <sertovicaida@gmail.com>
2022-12-14 19:51:58 +0100

    add javascript logic to functions

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,15 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date();
-    const expenseTitle = 'Car Insurance';
-    const expensePrice = 394.67;
-    return(
-       <div className="expense-item">
-        <div>{expenseDate.toISOString()}</div>
-        <div className="expense-item__description">
-            <h2>{expenseTitle}</h2>
-            <div className="expense-item__price">${expensePrice}</div>
-        </div>
-       </div>
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US', { month: 'long' })
+    return (
+        <><div>{month}</div><div className="expense-item">
+            <div>{props.date.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.price}</div>
+            </div>
+        </div></>
     )
 }
 
