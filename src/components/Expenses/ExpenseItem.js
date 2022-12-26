92fbe78d83237fdde0205df427331b0b2459f730
Aida Šertović Komić <sertovicaida@gmail.com>
2022-12-26 18:25:16 +0100

    manage title state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,18 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
+import { useState } from 'react';
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
     const clickHandler = () => {
-        console.log('Test');
+        setTitle('Updated!');
     }
-    
+
     return (
         <div className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.price}</div>
                 <button onClick={clickHandler}>Change Title</button>
             </div>
