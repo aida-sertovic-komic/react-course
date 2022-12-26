12c130fae0da8178c9e9d83fedbe3af1cecafd8f
Aida Šertović Komić <sertovicaida@gmail.com>
2022-12-26 13:53:29 +0100

    add button for changing title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,12 +2,17 @@ import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
+    const clickHandler = () => {
+        console.log('Test');
+    }
+    
     return (
         <div className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.price}</div>
+                <button onClick={clickHandler}>Change Title</button>
             </div>
         </div>
     )
